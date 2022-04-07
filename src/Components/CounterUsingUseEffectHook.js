ac8ef15a51ad28868cfad037ebffcdd07731ecf9
hanzalarohan123 <hanzalarohan@gmail.com>
2022-04-07 23:48:05 +0500

    useEffect hook

--- src/Components/CounterUsingUseEffectHook.js
+++ src/Components/CounterUsingUseEffectHook.js
@@ -4,6 +4,7 @@ import React,{useState,useEffect} from 'react'
 function CounterUsingUseEffectHook() 
 {
     const [count,setCount]=useState(0)
+    //Use Effect added
     useEffect(()=>{document.title=`${count}`})
 
   return (
